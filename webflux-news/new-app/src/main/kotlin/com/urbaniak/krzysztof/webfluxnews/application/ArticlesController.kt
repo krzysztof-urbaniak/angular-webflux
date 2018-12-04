71529e87585ee1e2d7fb5f364ada120311e7acce
Krzysztof Urbaniak <krzysztof.w.urbaniak@gmail.com>
2018-12-04 10:49:05 +0100

    cors

--- webflux-news/new-app/src/main/kotlin/com/urbaniak/krzysztof/webfluxnews/application/ArticlesController.kt
+++ webflux-news/new-app/src/main/kotlin/com/urbaniak/krzysztof/webfluxnews/application/ArticlesController.kt
@@ -2,6 +2,7 @@ package com.urbaniak.krzysztof.webfluxnews.application
 
 import io.swagger.annotations.ApiOperation
 import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -14,6 +15,7 @@ class ArticlesController(private val service: ArticlesService) {
         notes = "Articles are sent to the client as Server Sent Events",
         produces = MediaType.TEXT_EVENT_STREAM_VALUE
     )
+    @CrossOrigin(origins = ["http://localhost:4200"])
     @GetMapping("/news/{country}/{category}", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
     fun getArticles(
         @PathVariable(value = "country") country: String,
